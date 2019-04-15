ea2fb09103bedc9d7def5be2539e74751b6547a5
Gerald Bauer <gerald.bauer@gmail.com>
2019-04-15 02:34:40 +0200

    up

--- pubkey.rb
+++ pubkey.rb
@@ -6,14 +6,20 @@ privatekey = 1234
 
 # Elliptic curve multiplication
 group = ECDSA::Group::Secp256k1                          # Select the curve used in Bitcoin
-point = group.generator.multiply_by_scalar( privatekey ) # Multiply by integer (not hex)
+pp point = group.generator.multiply_by_scalar( privatekey ) # Multiply by integer (not hex)
+#=> <ECDSA::Point: secp256k1,
+#       0xe37648435c60dcd181b3d41d50857ba5b5abebe279429aa76558f6653f1658f2,
+#       0x6d2ee9a82d4158f164ae653e9c6fa7f982ed8c94347fc05c2d068ff1d38b304c>
 
-# Compressed format (even = 02, odd = 03)
+# Uncompressed format (with prefix 04)
+#   Convert to 64 hexstring characters (32 bytes) in length
+prefix = '04'
+pp pubkey = prefix + "%064x" % point.x + "%064x" % point.y
+#=> "04e37648435c60dcd181b3d41d50857ba5b5abebe279429aa76558f6653f1658f26d2ee9a82d4158f164ae653e9c6fa7f982ed8c94347fc05c2d068ff1d38b304c"
+
+# Compressed format (with prefix - even = 02, odd = 03)
 #   Instead of using both x and y coordinates,
 #   just use the x-coordinate and whether y is even/odd
 prefix = point.y % 2 == 0 ? '02' : '03'
-
-# Add the prefix to the x-coordinate
-#   Convert to hex (and make sure it is 32 bytes / 64 characters in length)
 pp pubkey = prefix + "%064x" % point.x
-# => "02e37648435c60dcd181b3d41d50857ba5b5abebe279429aa76558f6653f1658f2"
+#=> "02e37648435c60dcd181b3d41d50857ba5b5abebe279429aa76558f6653f1658f2"
